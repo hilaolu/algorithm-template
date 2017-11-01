055c3029b76606eed33f2acd973005af4ed0a3bc
hilaolu <i@hilaolu.com>
2017-11-01 12:23:46 +0800

    更新 'algorithm-template/堆排序.cpp'

--- algorithm-template/堆排序.cpp
+++ algorithm-template/堆排序.cpp
@@ -4,15 +4,15 @@ using namespace std;
 void heapdown(int s[],int i,int n){
 	int l,r,tmp;
 	while(true){
-		l=2i;r=2i+1;
+		l=2*i+1;r=2*i+2;
 		if(r>n){
-			if(l>n)return;
+			if(l>n)break;
 			if(s[l]>s[i]){
 				tmp=s[l];
 				s[l]=s[i];
 				s[i]=tmp;
 			}
-			return;
+			break;
 		}
 		if(s[l]>s[i]&&s[l]>s[r]){
 			tmp=s[l];
@@ -25,19 +25,28 @@ void heapdown(int s[],int i,int n){
 			tmp=s[r];
 			s[r]=s[i];
 			s[i]=tmp;
-			r=l;
+			i=r;
 			continue;
 		}
+		for(int j=0;j<n;j++)cout<<s[j]<<" ";
+		cout<<endl;
+	}
 }
 void heapsort(int s[],int n){
+	int tmp;
 	for(int i=(n+1)/2+1;i>0;i--)heapdown(s,i,n);
-	
+	for(int i=n-1;i>0;i--){
+		tmp=s[0];
+		s[0]=s[i];
+		s[i]=tmp;
+		heapdown(s,0,i);
+	}
 }
 int main(){
 	int n;
 	scanf("%d",&n);
 	int *s = new int[n];
-	for(int i=0;i<n;i++)scanf("%d",s[i]);
+	for(int i=0;i<n;i++)scanf("%d",&s[i]);
 	heapsort(s,n-1);
 	for(int i=0;i<n;i++)cout<<s[i]<<" ";
 	return 0;
